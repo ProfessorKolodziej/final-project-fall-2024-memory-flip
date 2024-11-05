20cd6333d5dd5fdc6c2d4d62329b5f8cd2045829
luyu0824 <luyu0824@bu.edu>
2024-11-04 19:30:58 -0500

    change html

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,13 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+function showScreen(screenId) {
+    // 隐藏所有界面
+    const screens = document.querySelectorAll('.screen');
+    screens.forEach(screen => screen.style.display = 'none');
+
+    // 显示选定的界面
+    document.getElementById(screenId).style.display = 'block';
+}
+
